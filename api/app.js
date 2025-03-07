321cb129ad1f25cad5204f14cd762ab4b884c09e
puneet-tanwar <puneet.tanwar@intimetec.com>
2025-03-07 11:20:05 +0530

    created error handling middleware

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -18,8 +19,17 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  const message = err.message || "Something went wrong";
+  return res.status(statusCode).send({
+    success: false,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
-
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+
+export const SignUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 12);
+  const newUser = new User({ username, email, password: hashedPassword });
+  console.log({ newUser });
+  try {
+    const savedUser = await newUser.save({ new: true });
+    console.log({ savedUser });
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: savedUser });
+  } catch (error) {
+    next(errorHandler(400, "Error creating user"));
+  }
+};
