1296dd2884d59cf3bff7993d1779039c5f23e4e9
puneet-tanwar <puneet.tanwar@intimetec.com>
2025-03-07 09:39:42 +0530

    added user model

--- api/app.js
+++ api/app.js
@@ -1,8 +1,25 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error.message);
+  });
 
 const app = express();
 
+app.use(express.json());
+app.use("/api/users", userRouter);
+
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
 
