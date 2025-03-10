bbcdf3968e1871e6361e66e1bc4d65d2a5b3e6af
puneet-tanwar <puneet.tanwar@intimetec.com>
2025-03-10 09:50:57 +0530

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import localStorage from "redux-persist/es/storage";
 import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
